e5cbb60863f4050a0eee405274d0d33b2a282e38
Joe Fowler <drjoefowler@gmail.com>
2018-04-09 15:49:02 -0600

    Add all the Twenties switches to the package.

--- switch_test.go
+++ switch_test.go
@@ -4,22 +4,11 @@ import (
 	"testing"
 )
 
-func TestSixes(t *testing.T) {
-	if len(sixesData) != 25 {
-		t.Errorf("len(sixesData) = %d, want 25", len(sixesData))
-	}
-	for i := 0; i < len(sixesData); i++ {
-		if len(sixesData[i]) != 6 {
-			t.Errorf("len(sixesData[%d]) = %d, want 6", i, len(sixesData[i]))
-		}
-	}
-}
-
 func TestInvert(t *testing.T) {
-	d := [][]int{{1, 2, 3, 4}, {3, 1, 2, 4}, {3, 2, 4, 1}, {1, 3, 2, 4}, {1, 3, 4, 2}}
-	sd := datamaker(d)
-	id := sd.invert()
+	data := [][]int{{1, 2, 3, 4}, {3, 1, 2, 4}, {3, 2, 4, 1}, {1, 3, 2, 4}, {1, 3, 4, 2}}
 	want := [][]int{{1, 2, 3, 4}, {2, 3, 1, 4}, {4, 2, 1, 3}, {1, 3, 2, 4}, {1, 4, 2, 3}}
+	sd := datamaker(data)
+	id := sd.invert()
 	for i := 0; i < 4; i++ {
 		for j := 0; j < 4; j++ {
 			if id[i][j] != want[i][j]-1 {
@@ -36,6 +25,7 @@ func TestSwitch(t *testing.T) {
 		t.Errorf("newSwitch with unequal length lines should raise error but did not")
 	}
 
+	// Test a few encipher/decipher positions from the sixes switch
 	s := sixesSwitch
 	var tests = []struct {
 		position    int
@@ -69,6 +59,34 @@ func TestSwitch(t *testing.T) {
 		s.step()
 		if s.position != i+1 {
 			t.Errorf("sixesSwitch is at position %d, want %d", s.position, i+1)
+		}
+	}
+
+	// Test a few encipher/decipher positions from the twenties switch #1.
+	s = twenties1
+	var tests20_1 = []struct {
+		position    int
+		permutation []int
+	}{
+		{0, []int{6, 19, 14, 1, 10, 4, 2, 7, 13, 9, 8, 16, 3, 18, 15, 11, 5, 12, 20, 17}},
+		{1, []int{4, 5, 16, 17, 14, 1, 20, 15, 3, 8, 18, 11, 12, 13, 10, 19, 2, 6, 9, 7}},
+		{9, []int{12, 8, 17, 9, 3, 20, 4, 10, 14, 5, 7, 18, 2, 16, 13, 6, 1, 19, 15, 11}},
+	}
+
+	for _, test := range tests20_1 {
+		s.setPosition(test.position)
+
+		for i, want := range test.permutation {
+			p := s.decipher(i)
+			if p != want-1 {
+				t.Errorf("twenties1 at pos=%d level %d deciphers to %d, want %d",
+					s.position, i, p, want-1)
+			}
+			c := s.encipher(p)
+			if c != i {
+				t.Errorf("twenties1 at pos=%d level %d enciphers to %d, want %d",
+					s.position, p, c, i)
+			}
 		}
 	}
 }
